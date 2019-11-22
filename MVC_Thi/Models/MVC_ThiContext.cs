e9877070dd73078e8cd03d89800c10c7c292be45
nguyen128 <tvngiagia@gmail.com>
2019-11-22 19:30:49 +0700

    Done thi MVC

---
+++ MVC_Thi/Models/MVC_ThiContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Thi.Models
+{
+    public class MVC_ThiContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MVC_ThiContext() : base("name=MVC_ThiContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MVC_Thi.Models.Product> Products { get; set; }
+
+        public System.Data.Entity.DbSet<MVC_Thi.Models.Category> Categories { get; set; }
+    }
+}
